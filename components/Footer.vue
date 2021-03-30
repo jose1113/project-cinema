9b12969b21947dbb1b1c71307b9011bcb84d9058
jose1113 <joseigor113@gmail.com>
2021-03-30 17:49:40 -0300

    fix footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,20 +1,19 @@
 <template>
-  <footer>
+  <footer class="text-white">
     <hr class="container">
-    <b-nav class="container">
-      <b-nav-text>Filmes</b-nav-text>
-        <nuxt-link to="#"><b-nav-item href="#1">Lançamentos</b-nav-item></nuxt-link>
-        <nuxt-link to="#"><b-nav-item href="#1">Em breve</b-nav-item></nuxt-link>
+    <b-nav class="container d-flex justify-content-center">
+        <nuxt-link to="#" class="text-white mr-5">Lançamentos</nuxt-link>
+        <nuxt-link to="#" class="text-white">Em breve</nuxt-link>
     </b-nav>
-    <hr class="container">
-    <b-nav class="d-flex justify-content-center align-items-center">
-      <nuxt-link to="/">
-        <b-nav-item>
-          <img src="~/assets/logo.png" alt="Logo do site">
-        </b-nav-item>
+    <hr class="container w-50 bg-white">
+    <div class="container w-50 d-flex align-items-center justify-content-between">
+      <nuxt-link to="#" class="container text-white">
+        <b-nav-text>Cinema</b-nav-text>
       </nuxt-link>
-      <b-nav-text>Copyright © 2021 Cinema</b-nav-text>
-    </b-nav>
+      <div class="container">
+        <b-nav-text class="d-flex justify-content-end">Copyright © 2021</b-nav-text>
+      </div>
+    </div>
   </footer>
 </template>
 
@@ -25,5 +24,6 @@ img{
 }
 footer{
   bottom: 0;
+  background-color: #414141;
 }
 </style>
