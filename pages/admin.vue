00b9d91264edd4bcb8bcaf1d717449f2eed3e747
jose1113 <joseigor113@gmail.com>
2021-04-02 11:54:08 -0300

    add page admin

---
+++ pages/admin.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <div class="container mt-3">
+      <h1>Adicionar filme</h1>
+    </div>
+    <form class="container mt-3">
+      <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+      <b-form-group
+        id="input-group-1"
+        label="Nome:"
+        label-for="input-1"
+      >
+        <b-form-input
+          id="input-1"
+          v-model="form.name"
+          type="text"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-3" label-for="input-3">
+        <b-form-select
+          id="input-3"
+          v-model="form.img"
+          :options="img"
+          required
+        ></b-form-select>
+      </b-form-group>
+
+      <b-button type="submit" variant="primary">Submit</b-button>
+      <b-button type="reset" variant="danger">Reset</b-button>
+    </b-form>
+    </form>
+  </div>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        form: {
+          name: '',
+          food: null,
+          checked: []
+        },
+        img: [{ text: 'Imagem', value: null }, '~/assets/'],
+        show: true
+      }
+    },
+    methods: {
+      onSubmit(event) {
+        event.preventDefault()
+        alert(JSON.stringify(this.form))
+      },
+      onReset(event) {
+        event.preventDefault()
+        // Reset our form values
+        this.form.name = ''
+        this.form.img = null
+        // Trick to reset/clear native browser form validation state
+        this.show = false
+        this.$nextTick(() => {
+          this.show = true
+        })
+      }
+    }
+  }
+</script>
