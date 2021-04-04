562e27fdb16c4294b50bd2f7cb0b62b7e13f29b7
jose1113 <joseigor113@gmail.com>
2021-04-04 20:08:10 -0300

    fix responsive

--- components/Social-header.vue
+++ components/Social-header.vue
@@ -1,23 +1,34 @@
 <template>
-  <div class="container mt-1">
-    <b-nav>
-      <b-navbar>Nossas redes</b-navbar>
-      <b-nav-item>
-        <img src="~/assets/logo-facebook.png" alt="Logotipo do Facebook">
-      </b-nav-item>
-      <b-nav-item>
-        <img src="~/assets/logo-instagram.png" alt="Logotipo do Instagram">
-      </b-nav-item>
-      <b-navbar>
-        <nuxt-link to="#">
-          Contraste
-        </nuxt-link>
-      </b-navbar>
-    </b-nav>
-</div>
+  <div class="complete">
+    <b-container>
+      <b-row>
+        <b-col cols="6" sm="6" md="6" lg="6" xl="6">
+          <b-navbar class="text-white">
+            <p class="m-1">Nossas redes</p>
+            <a href="#">
+              <img class="m-2" src="~/assets/logo-facebook.png" alt="Logotipo do Facebook">
+              </a>
+            <a href="#">
+              <img src="~/assets/logo-instagram.png" alt="Logotipo do Instagram">
+            </a>
+            </b-navbar>
+        </b-col>
+        <b-col cols="6" sm="6" md="6" lg="6" xl="6">
+          <b-navbar class="m-2 d-flex justify-content-end">
+            <nuxt-link to="#">
+              Contraste
+            </nuxt-link>
+          </b-navbar>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
 </template>
 
 <style scoped>
+.complete{
+  background-color: rgb(189, 187, 187);
+}
 img{
   width: 20px;
   height: 20px;
