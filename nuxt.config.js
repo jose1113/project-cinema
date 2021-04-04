fff2b6588122f4b9f6075818ceb2c959a6cc9494
jose1113 <joseigor113@gmail.com>
2021-04-04 12:18:34 -0300

    babel compact false

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,5 +44,8 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-  }
+  },
+  loaders: [
+    { test: /\.js$/, loader: 'babel', query: {compact: false} },
+  ]
 }
