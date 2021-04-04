3ab1faf713cc5fbfff5acd880d8dd9d8f09bdd17
jose1113 <joseigor113@gmail.com>
2021-04-04 20:07:31 -0300

    fix menu

--- components/Menu.vue
+++ components/Menu.vue
@@ -1,40 +1,45 @@
 <template>
-<div>
-  <b-navbar class="nav" toggleable="lg" type="dark">
-    <b-navbar-brand href="/">Cinema</b-navbar-brand>
+  <div>
+    <b-navbar class="nav" toggleable="lg" type="dark">
+      <b-navbar-brand href="/">Cinema</b-navbar-brand>
 
-    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-    <b-collapse id="nav-collapse" is-nav>
-    <b-nav-item href="#">Lançamentos</b-nav-item>
-    <b-nav-item href="#">Em breve</b-nav-item>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-collapse id="nav-collapse" is-nav>
+      <b-nav-item href="#" class="lancamentos">Lançamentos</b-nav-item>
+      <b-nav-item href="#" class="lancamentos">Em breve</b-nav-item>
 
-      <!-- Right aligned nav items -->
-      <b-navbar-nav class="ml-auto">
-        <b-nav-form>
-          <b-form-input size="sm" class="mr-sm-2" placeholder=""></b-form-input>
-          <b-button size="sm" class="my-2 my-sm-0" type="submit">Pesquisar</b-button>
-        </b-nav-form>
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-form>
+            <b-form-input size="sm" class="mr-sm-2"></b-form-input>
+            <b-button size="sm" class="my-2 my-sm-0" type="submit">Pesquisar</b-button>
+          </b-nav-form>
 
-        <b-nav-item-dropdown text="Lang" right>
-          <b-dropdown-item href="#">EN</b-dropdown-item>
-          <b-dropdown-item href="#">ES</b-dropdown-item>
-        </b-nav-item-dropdown>
-      </b-navbar-nav>
-    </b-collapse>
-  </b-navbar>
-</div>
+          <b-nav-item-dropdown text="Lang" right>
+            <b-dropdown-item href="#">EN</b-dropdown-item>
+            <b-dropdown-item href="#">ES</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
 </template>
 
 <style scoped>
-.dropdown{
-  text-decoration: none;
-  list-style: none;
-  color: white;
-}
-.nav{
-  background-color: #af0404;
-}
-::v-deep .nav-link {
-   color: white!important;
-}
+  .lancamentos:hover{
+    background-color: gray;
+    color: black!important;
+    border-radius: 5px 5px 5px 5px;
+  }
+  .dropdown{
+    text-decoration: none;
+    list-style: none;
+    color: white;
+  }
+  .nav{
+    background-color: #af0404;
+  }
+  ::v-deep .nav-link {
+    color: white!important;
+  }
 </style>
