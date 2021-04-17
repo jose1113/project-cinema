3cb62cba120311969b6316d46f8be8ed5b07f75f
jose1113 <joseigor113@gmail.com>
2021-04-17 15:40:29 -0300

    add props

--- pages/actor-carey-mulligan.vue
+++ pages/actor-carey-mulligan.vue
@@ -1,33 +1,17 @@
 <template>
   <div>
     <b-breadcrumb :items="items"></b-breadcrumb>
-    <b-container>
-      <b-row class="d-flex align-items-center">
-        <b-col>
-          <h3 class="mb-3">Carey Mulligan</h3>
-          <b-avatar src="~/assets/actor-carey-mulligan.jpg" size="300px"></b-avatar>
-        </b-col>
-        <b-col>
-          <p><strong>Atividades:</strong> <span>Atriz</span></p>
-          <p><strong>Nome de nascimento: </strong><span>Carey Hannah Mulligan</span></p>
-          <p><strong>Nascimento: </strong><span>28 de maio de 1985 (Londres, Inglaterra)</span></p>
-          <p><strong>Nacionalidade: </strong><span>Britânica</span></p>
-          <p><strong>Idade: </strong><span>35 anos</span></p>
-        </b-col>
-      </b-row>
-    </b-container>
-    <b-container>
-      <b-row class="mt-3">
-        <b-col class="border text-center">
-          <strong>14</strong>
-          <p>Anos de carreira</p>
-        </b-col>
-        <b-col class="border text-center">
-          <strong>23</strong>
-          <p>Filmes e séries lançados</p>
-        </b-col>
-      </b-row>
-    </b-container>
+    <Actor
+      nome = 'Carey Mulligan'
+      atividades = 'Atriz'
+      nomeNasc = 'Carey Hannah Mulligan'
+      nascimento = '28 de maio de 1985 (Londres, Inglaterra)'
+      nacionalidade = 'Britânica'
+      idade = '35 anos'
+      anosCar = '14'
+      filmes = '23'
+      img = 'actor-carey-mulligan.jpg'
+    />
   </div>
 </template>
 <style scoped>
