1b21d00af0028d230a9d8d953e899f38a306f80e
jose1113 <joseigor113@gmail.com>
2021-04-19 17:14:38 -0300

    add logo in svg to Menu

---
+++ pages/teste.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <Filme
+      title = 'Filme novo'
+      category = 'Terror'
+      year = '2021'
+      age = '14'
+      duration = '1h e 20min'
+      sinopse = 'Lorem'
+    />
+  </div>
+</template>
