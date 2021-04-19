54314a16dd4a734dd8add48bab3af62b3dff07f4
jose1113 <joseigor113@gmail.com>
2021-04-19 15:47:11 -0300

    add component Actor in this page

--- pages/actor-lily-james.vue
+++ pages/actor-lily-james.vue
@@ -1,33 +1,17 @@
 <template>
   <div>
     <b-breadcrumb :items="items"></b-breadcrumb>
-    <b-container>
-      <b-row class="d-flex align-items-center">
-        <b-col>
-          <h3 class="mb-3">Lily James</h3>
-          <b-avatar src="~/assets/actor-lily-james.jpg" size="300px"></b-avatar>
-        </b-col>
-        <b-col>
-          <p><strong>Atividades:</strong> <span>Atriz</span></p>
-          <p><strong>Nome de nascimento: </strong><span>Lily Chloe Ninette Thomson</span></p>
-          <p><strong>Nascimento: </strong><span>5 de abril de 1989 (Esher, Inglaterra)</span></p>
-          <p><strong>Nacionalidade: </strong><span>Britânica</span></p>
-          <p><strong>Idade: </strong><span>31 anos</span></p>
-        </b-col>
-      </b-row>
-    </b-container>
-    <b-container>
-      <b-row class="mt-3">
-        <b-col class="border text-center">
-          <strong>9</strong>
-          <p>Anos de carreira</p>
-        </b-col>
-        <b-col class="border text-center">
-          <strong>19</strong>
-          <p>Filmes e séries lançados</p>
-        </b-col>
-      </b-row>
-    </b-container>
+    <Actor
+      nome = 'Lily James'
+      atividades = 'Atriz'
+      nomeNasc = 'Lily Chloe Ninette Thomso'
+      nascimento = '5 de abril de 1989 (Esher, Inglaterra)'
+      nacionalidade = 'Britânica'
+      idade = '31 anos'
+      anosCar = '9'
+      filmes = '19'
+      img = 'actor-carey-mulligan.jpg'
+    />
   </div>
 </template>
 <style scoped>
