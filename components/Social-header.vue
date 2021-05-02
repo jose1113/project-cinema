925fc52d24439d2097c56f851c59285722d882b8
jose1113 <joseigor113@gmail.com>
2021-05-02 16:10:20 -0300

    styling

--- components/Social-header.vue
+++ components/Social-header.vue
@@ -4,7 +4,7 @@
       <b-row>
         <b-col cols="6" sm="6" md="6" lg="6" xl="6">
           <b-navbar class="text-white">
-            <p class="m-1">{{ $t('rede') }}</p>
+            <p class="m-1" style="font-weight:500;">{{ $t('rede') }}</p>
             <a href="#">
               <img class="m-2" src="~/assets/logo-facebook.png" alt="Logotipo do Facebook">
               </a>
@@ -15,7 +15,7 @@
         </b-col>
         <b-col cols="6" sm="6" md="6" lg="6" xl="6">
           <b-navbar class="m-2 d-flex justify-content-end">
-            <nuxt-link to="#">
+            <nuxt-link to="#" style="color:#B5121B; font-weight:600;">
               {{ $t('contrast') }}
             </nuxt-link>
           </b-navbar>
@@ -27,7 +27,7 @@
 
 <style scoped>
 .complete{
-  background-color: #949494;
+  background-color: #c9c9c9;
 }
 img{
   width: 20px;
