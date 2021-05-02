ba0bcd5cd7842ea63f36ab344e252d3ec08f2406
jose1113 <joseigor113@gmail.com>
2021-05-02 16:09:44 -0300

    fix responsive

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,19 +1,30 @@
 <template>
-  <footer class="text-white">
+  <footer class="text-white rounded-top">
     <hr class="container">
-    <b-nav class="container d-flex justify-content-center">
-        <nuxt-link to="#" class="text-white mr-5">{{ $t('lancamentos') }}</nuxt-link>
-        <nuxt-link to="#" class="text-white">{{ $t('comingSoon') }}</nuxt-link>
-    </b-nav>
-    <hr class="container w-50 bg-white">
-    <div class="container w-50 d-flex align-items-center justify-content-between">
-      <nuxt-link to="#" class="container text-white">
-        <b-nav-text>Cinema</b-nav-text>
-      </nuxt-link>
-      <div class="container">
-        <b-nav-text class="d-flex justify-content-end">Copyright © 2021</b-nav-text>
-      </div>
-    </div>
+    <b-container>
+      <b-row class="text-center d-flex align-items-center">
+        <b-col cols="4" sm="4" md="4" lg="4" xl="4">
+          <b-navbar-brand href="/">
+            <img src="~/assets/logo.png" alt="Logotipo" class="logo">
+          </b-navbar-brand>
+        </b-col>
+        <b-col cols="4" sm="4" md="4" lg="4" xl="4">
+          <nuxt-link to="#" class="text-white mr-5" style="font-weight:600;">{{ $t('lancamentos') }}</nuxt-link>
+          <nuxt-link to="#" class="text-white" style="font-weight:600;">{{ $t('comingSoon') }}</nuxt-link>
+        </b-col>
+      </b-row>
+      <hr class="container w-50 bg-white">
+      <b-row class="text-center">
+        <b-col class="container w-50" cols="6" sm="6" md="6" lg="6" xl="6">
+          <nuxt-link to="#" class="container text-white">
+            <b-nav-text>Cinema</b-nav-text>
+          </nuxt-link>
+        </b-col>
+        <b-col class="container" cols="6" sm="6" md="6" lg="6" xl="6">
+            <b-nav-text>Copyright © 2021</b-nav-text>
+        </b-col>
+      </b-row>
+    </b-container>
   </footer>
 </template>
 
@@ -24,6 +35,16 @@ img{
 }
 footer{
   bottom: 0;
-  background-color: #414141;
+  background-image: linear-gradient(to right bottom, #414141, #4f4f4f, #5d5d5d, #6d6b6c, #7c7a7a);
+}
+.logo{
+  width: 100px;
+  height: 100px;
+}
+@media(max-width:576px){
+  .logo{
+    width: 80px;
+    height: 80px;
+  }
 }
 </style>
