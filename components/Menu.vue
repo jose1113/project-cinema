239fbfbf1065b3d670fd45299b8ca2034a23ddec
jose1113 <joseigor113@gmail.com>
2021-05-22 18:48:20 -0300

    adding correct link

--- components/Menu.vue
+++ components/Menu.vue
@@ -8,7 +8,7 @@
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
       <b-collapse id="nav-collapse" is-nav>
       <b-nav-item href="#" class="lancamentos">{{ $t('lancamentos') }}</b-nav-item>
-      <b-nav-item href="#" class="lancamentos">{{ $t('comingSoon') }}</b-nav-item>
+      <b-nav-item href="/em-breve" class="lancamentos">{{ $t('comingSoon') }}</b-nav-item>
 
         <!-- Right aligned nav items -->
         <b-navbar-nav class="ml-auto">
