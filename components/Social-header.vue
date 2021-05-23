a081b5052448fe119ebd39afda979388c66a0294
jose1113 <joseigor113@gmail.com>
2021-05-23 03:52:36 -0300

    improving pages

--- components/Social-header.vue
+++ components/Social-header.vue
@@ -7,17 +7,10 @@
             <p class="m-1" style="font-weight:500;">{{ $t('rede') }}</p>
             <a href="#">
               <img class="m-2" src="~/assets/logo-facebook.png" alt="Logotipo do Facebook">
-              </a>
+            </a>
             <a href="#">
               <img src="~/assets/logo-instagram.png" alt="Logotipo do Instagram">
             </a>
-            </b-navbar>
-        </b-col>
-        <b-col cols="6" sm="6" md="6" lg="6" xl="6">
-          <b-navbar class="m-2 d-flex justify-content-end align-items-center">
-            <p style="color:#B5121B; font-weight:600; cursor:pointer;">
-              <ColorModePicker />
-            </p>
           </b-navbar>
         </b-col>
       </b-row>
