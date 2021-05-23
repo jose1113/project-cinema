dd37be893858656ad3336d087c23a2bfd92c4457
jose1113 <joseigor113@gmail.com>
2021-05-23 17:59:41 -0300

    new component AboutUs

---
+++ components/AboutUs.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <b-container class="d-flex flex-column p-4">
+      <b-row class="m-auto">
+        <b-col>
+          <h4 class="mb-3">{{ $t('aboutUs') }}</h4>
+          <iframe width="790" height="444" class="rounded" src="https://www.youtube.com/embed/EIgbecUZQgc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+<style scoped>
+h4{
+  font-weight: bold;
+  font-size: 34px;
+}
+</style>
